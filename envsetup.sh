bd90f61adc7705fd866c912ac9c4d51f134b27ed
Arnav Gosain <gosain.arnav@gmail.com>
2014-10-01 16:25:54 -0400

    brunch: use time
    Change-Id: Ie0da5bd1593cd638c543d942fd2e8fb527e2ef94

--- envsetup.sh
+++ envsetup.sh
@@ -490,7 +490,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        time mka bacon
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
